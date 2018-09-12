05a94765cf1b0729fc2b339dcd65d8c589f67d1d
Christian Katzorke <c.katzorke@web.de>
2018-09-12 16:58:03 +0200

    moving to src

---
+++ src/avscfs.js
@@ -0,0 +1,75 @@
+/**
+ * Filesystemhelpers for collecting and reading and parsing avsc files
+ */
+
+
+const path = require('path');
+const fs = require('fs');
+const sys = require('util');
+
+/**
+ * Reads in the given file and parses as json
+ * @param {string} filename
+ * @returns {object}
+ */
+function readJSON(filename) {
+    let json, parsed;
+    console.debug('Parsing ', filename);
+    json = fs.readFileSync(path.resolve(process.cwd(), filename), 'utf-8');
+    try {
+        parsed = JSON.parse(json);
+    } catch (e) {
+        sys.error('Not a valid json file: ' + filename);
+        process.exit(1);
+    }
+    return parsed;
+}
+
+/**
+ * Steps through the given folder and recursively collects all avsc files.
+ * @param {string} folder the input folder to iterate and gather all found *.avsc files
+ * @returns {Array<string>} the list with all found inputfiles
+ */
+function collectInputFiles(folder) {
+    let files = new Array();
+    console.log('Input dir: ', folder);
+    let dirEntries = fs.readdirSync(path.resolve(process.cwd(), folder), {
+        withFileTypes: true
+    });
+    dirEntries.forEach((entry) => {
+        if (entry.isFile()) {
+            let file = folder + '/' + entry.name;
+            console.debug('adding file', file);
+            if(file.endsWith('.avsc')){
+                files.push(file);
+            } else {
+                console.debug(`Ignoring ${file}, not an avro schema file (.avsc)`);
+            }
+        } else if (entry.isDirectory()) {
+            let subfolder = folder + '/' + entry.name;
+            console.debug('Digging into ', subfolder);
+            files.push(...collectInputFiles(subfolder));
+        }
+    })
+    return files;
+}
+
+function writeAvroDoc(outputFile, html){
+    if (outputFile.indexOf('/') > -1) {
+        let outFolder = path.resolve(process.cwd(), outputFile.substring(0, outputFile.lastIndexOf('/')));
+        if(!fs.existsSync(outFolder)){
+            fs.mkdirSync(outFolder);
+        }
+    }
+    fs.writeFile(outputFile, html, function (err) {
+        if (err) {
+            throw err;
+        } else {
+            console.log('Avrodoc saved to ' + outputFile);
+        }
+    });
+}
+
+exports.writeAvroDoc = writeAvroDoc;
+exports.readFileAsJSON = readJSON;
+exports.collectInputFiles = collectInputFiles;
---
+++ src/cli.js
@@ -0,0 +1,58 @@
+/*jshint node:true */
+
+/**
+ * CLI usage for avrodoc-plus
+ * Usage:
+ * avrodoc-plus -i inputfolder -o outputfile
+ * avrodoc-plus inputfile1 inputfile2... -o outputfile
+ * Example:
+ * avrodoc-plus -i schemata -o doc.html
+ * avrodoc-plus schemata/user.avsc schemata/account.avsc -o userdoc.html
+ */
+
+const avscfs = require('./avscfs'),
+    content = require('./static_content'),
+    fs = require('fs'),
+    sys = require('util'),
+    argv = require('optimist').alias('o', 'output').alias('i', 'input').argv;
+
+let inputFiles = null;
+let outputFile = null;
+
+// Determine list of input files file1.avsc file2.avsc
+if (argv.input) {
+    console.debug('Collecting all avsc files from root folder ', argv.input);
+    inputFiles = avscfs.collectInputFiles(argv.input);
+} else if (argv._) {
+    console.debug('Using passed arguments as inputfiles...');
+    inputFiles = argv._;
+}
+
+// Determine whether an output file is specified
+if (argv.output) {
+    outputFile = argv.output;
+}
+
+//valid input?
+if (!inputFiles || inputFiles.length === 0 || outputFile == null) {
+    sys.error('Usage: avrodoc [-i rootfolder] [my-schema.avsc [another-schema.avsc...]] [-o=my-documentation.html]');
+    process.exit(1);
+}
+
+
+var schemata = inputFiles.map(function (filename) {
+    return {
+        json: avscfs.readFileAsJSON(filename),
+        filename: filename
+    };
+});
+
+content.topLevelHTML({
+    inline: true,
+    schemata: schemata
+}, function (err, html) {
+    if (err) {
+        throw err;
+    }
+    avscfs.writeAvroDoc(outputFile, html);
+});

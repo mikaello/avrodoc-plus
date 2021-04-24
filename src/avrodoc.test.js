1643599a293e51b1e875e582101f45885d03d71c
mikaello <2505178+mikaello@users.noreply.github.com>
2021-04-24 07:43:11 +0200

    test: test that HTML document is created with content

--- src/avrodoc.test.js
+++ src/avrodoc.test.js
@@ -1,3 +1,21 @@
-test('avrodoc creates documentation', () => {
-    expect(true).toBe(true);
+import { createAvroDoc } from "./avrodoc.js";
+import { readFileSync, unlinkSync } from "fs";
+
+describe("test HTML generation", () => {
+  const testFile = "jestTest.html";
+
+  afterAll(() => {
+    // Cleanup
+    unlinkSync(testFile);
+  });
+
+  test("avrodoc creates documentation", async () => {
+    await expect(
+      createAvroDoc([], ["./schemata/example.avsc"], testFile)
+    ).resolves.toBeUndefined();
+
+    expect(readFileSync(testFile, "utf-8")).toContain(
+      '<!DOCTYPE html><html><head><meta charset="UTF-8"><title>Avrodoc</title>'
+    );
+  });
 });

3b6c78d5a522b3bc421274170f44dd37262aa0d6
mikaello <2505178+mikaello@users.noreply.github.com>
2026-04-12 22:31:47 +0200

    chore: apply prettier formatting

--- src/avrodoc.test.js
+++ src/avrodoc.test.js
@@ -19,11 +19,7 @@ describe("test HTML generation", () => {
       testFile,
     );
 
-    assert.ok(
-      readFileSync(testFile, "utf-8").includes(
-        'data-route="#/"',
-      ),
-    );
+    assert.ok(readFileSync(testFile, "utf-8").includes('data-route="#/"'));
   });
 });
 
